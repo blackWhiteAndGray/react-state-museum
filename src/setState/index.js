e214ae93caa4c0655bae733b0e6b342e4b75201b
pangfeng <pangfeng22@gmail.com>
2019-12-03 21:58:56 +0800

    develop setState

---
+++ src/setState/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import ListItems from "./Components/listItems";
+import AddItems from "./Components/addItem";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);;
+    this.state = {
+      allItems: ["nachos", "burritos", "hot dog"],
+      newItemName: ""
+    };
+  }
+
+  addItem = () => {
+    this.setState(state => ({
+      allItems: [...state.allItems, state.newItemName],
+      newItemName: ""
+    }));
+  };
+
+  setNewItemName = event => {
+    this.setState({ newItemName: event.target.value });
+  };
+
+  clear = () => {
+    this.setState({ allItems: [] });
+  };
+
+  render() {
+    return (
+      <div style={styles}>
+        <h2>Welcome to setState</h2>
+        <AddItems
+          addItem={this.addItem}
+          setNewItemText={this.setNewItemName}
+          value={this.state.newItemName}
+          clear={this.clear}
+        />
+        <ListItems allItems={this.state.allItems} />
+      </div>
+    );
+  }
+}
